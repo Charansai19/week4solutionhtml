acda4c78cd0af1bbf152552da3115a0937968e54
Charansai19 <81897450+Charansai19@users.noreply.github.com>
2021-04-12 15:29:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var names =["charan","worthythor", "johannson", "scarlett","jimmyrich", 
+"jaddu","Jaffa","Jadeja","ashok","laptop"];
+
+for (var i = 0; i < names.length;i++) {
+	if(names[i].charAt(0)==="J" || names[i].charAt(0)==="j"){
+		console.log("goodbye   "+names[i]);
+	}
+	else
+		{
+			( console.log("hello   "+names[i]));
+		}
+
+}
